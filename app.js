56fcd0d1de8d430e1e3be811b0809248e05d972c
thpss91103 <a0975142932@gmail.com>
2023-04-09 15:32:45 +0800

    feat: start project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+// const routes = require('./routes')
+
+const app = express()
+const port = 3000
+
+// require('./config/mongoose')
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// app.use(routes)
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
