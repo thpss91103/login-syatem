5e2f7af8073810b3f1f38e57f31d8a322212210e
thpss91103 <a0975142932@gmail.com>
2023-04-09 17:27:28 +0800

    feat: add login function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const userList = require('../../models/userList')
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+  const email = req.body.email
+  const pwd = req.body.password
+  const error = 'Your email or password is fail.'
+
+  userList.findOne({ email })
+    .lean()
+    .then(data => {
+      if (data === null) {
+        return res.render('index', { error, email })
+      }
+      if (data.password === pwd) {
+        return res.render('user', {name: data.firstName})
+      } else {
+        return res.render('index', { error, email})
+      }
+    })
 })
 
 module.exports = router
